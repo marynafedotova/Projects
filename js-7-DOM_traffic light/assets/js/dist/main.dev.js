ddc34a1437e0d7968bbe9c4943c364e7c8f7bdf0
marynafedotova <mfedotova151@gmail.com>
2024-02-01 22:27:39 +0200

    fix projecr ftraffic light

--- js-7-DOM_traffic light/assets/js/dist/main.dev.js
+++ js-7-DOM_traffic light/assets/js/dist/main.dev.js
@@ -4,20 +4,16 @@ var currentLightIndex = 0;
 var lights = document.querySelectorAll('.light');
 
 function changeLight() {
-  // Видаляємо клас "active" з поточного світлодіоду
-  lights[currentLightIndex].classList.remove('active'); // Змінюємо поточний індекс для світлодіода
-
-  currentLightIndex = (currentLightIndex + 1) % lights.length; // Додаємо клас "active" та відповідний клас для кольору новому світлодіоду
-
+  lights[currentLightIndex].classList.remove('active');
+  currentLightIndex = (currentLightIndex + 1) % lights.length;
   lights[currentLightIndex].classList.add('active');
   updateColor();
 }
 
 function updateColor() {
-  // Забираємо всі класи кольорів усіх світлодіодів
   lights.forEach(function (light) {
     light.classList.remove('red', 'yellow', 'green');
-  }); // Додаємо відповідний клас кольору до нового світлодіода
+  });
 
   if (currentLightIndex === 0) {
     lights[currentLightIndex].classList.add('red');
--- js-7-DOM_traffic light/assets/js/main.js
+++ js-7-DOM_traffic light/assets/js/main.js
@@ -1,25 +1,15 @@
 let currentLightIndex = 0;
 const lights = document.querySelectorAll('.light');
-
 function changeLight() {
-  // Видаляємо клас "active" з поточного світлодіоду
   lights[currentLightIndex].classList.remove('active');
-
-  // Змінюємо поточний індекс для світлодіода
   currentLightIndex = (currentLightIndex + 1) % lights.length;
-
-  // Додаємо клас "active" та відповідний клас для кольору новому світлодіоду
   lights[currentLightIndex].classList.add('active');
   updateColor();
 }
-
 function updateColor() {
-  // Забираємо всі класи кольорів усіх світлодіодів
   lights.forEach(light => {
     light.classList.remove('red', 'yellow', 'green');
   });
-
-  // Додаємо відповідний клас кольору до нового світлодіода
   if (currentLightIndex === 0) {
     lights[currentLightIndex].classList.add('red');
   } else if (currentLightIndex === 1) {

d9b040f0abede6e45d85fa2047aa7b633b1b5128
marynafedotova <mfedotova151@gmail.com>
2024-02-01 22:04:40 +0200

    add 7-3 project traffict light

--- js-7-DOM_traffic light/assets/js/dist/main.dev.js
+++ js-7-DOM_traffic light/assets/js/dist/main.dev.js
@@ -1,25 +1,24 @@
-let currentLightIndex = 0;
-const lights = document.querySelectorAll('.light');
+"use strict";
+
+var currentLightIndex = 0;
+var lights = document.querySelectorAll('.light');
 
 function changeLight() {
   // Видаляємо клас "active" з поточного світлодіоду
-  lights[currentLightIndex].classList.remove('active');
+  lights[currentLightIndex].classList.remove('active'); // Змінюємо поточний індекс для світлодіода
 
-  // Змінюємо поточний індекс для світлодіода
-  currentLightIndex = (currentLightIndex + 1) % lights.length;
+  currentLightIndex = (currentLightIndex + 1) % lights.length; // Додаємо клас "active" та відповідний клас для кольору новому світлодіоду
 
-  // Додаємо клас "active" та відповідний клас для кольору новому світлодіоду
   lights[currentLightIndex].classList.add('active');
   updateColor();
 }
 
 function updateColor() {
   // Забираємо всі класи кольорів усіх світлодіодів
-  lights.forEach(light => {
+  lights.forEach(function (light) {
     light.classList.remove('red', 'yellow', 'green');
-  });
+  }); // Додаємо відповідний клас кольору до нового світлодіода
 
-  // Додаємо відповідний клас кольору до нового світлодіода
   if (currentLightIndex === 0) {
     lights[currentLightIndex].classList.add('red');
   } else if (currentLightIndex === 1) {

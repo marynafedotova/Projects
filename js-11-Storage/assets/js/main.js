8520c87db1cf944fca1c15d0b5f7c71944d271f0
marynafedotova <mfedotova151@gmail.com>
2024-02-22 21:19:18 +0200

    add homework weather, 11 local storage

--- js-11-Storage/assets/js/main.js
+++ js-11-Storage/assets/js/main.js
@@ -1,30 +1,23 @@
 
-
 document.addEventListener('DOMContentLoaded', function() {
-  // Функція для отримання погоди з OpenWeather API
   function getWeather() {
       const apiKey = 'db760a385a63fc11b7a8fad5b0bc0637';
-      const city = 'Lviv'; // Замініть на назву вашого міста
+      const city = 'Lviv';
       const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
-
-      // Перевірка, чи маємо збережені дані в localStorage та чи не минуло 2 години з останнього запиту
       const storedWeather = localStorage.getItem('weatherData');
       if (storedWeather) {
           const { timestamp, data } = JSON.parse(storedWeather);
           const currentTime = new Date().getTime();
-          const twoHours = 2 * 60 * 60 * 1000; // 2 години в мілісекундах
+          const twoHours = 2 * 60 * 60 * 1000;
 
           if (currentTime - timestamp < twoHours) {
               displayWeather(data);
               return;
           }
       }
-
-      // Виконання запиту до OpenWeather API
       fetch(apiUrl)
           .then(response => response.json())
           .then(data => {
-              // Зберігання отриманих даних в localStorage
               const currentTime = new Date().getTime();
               const weatherData = { timestamp: currentTime, data: data };
               localStorage.setItem('weatherData', JSON.stringify(weatherData));
@@ -33,18 +26,14 @@ document.addEventListener('DOMContentLoaded', function() {
           })
           .catch(error => console.error('Error fetching weather:', error));
   }
-
-  // Функція для відображення інформації про погоду на сторінці
   function displayWeather(data) {
       const weatherContainer = document.getElementById('weather-container');
       const weatherInfo = document.getElementById('weather-info');
 
-      const temperature = Math.round(data.main.temp - 273.15); // Конвертація з Кельвінів в градуси Цельсія
+      const temperature = Math.round(data.main.temp - 273.15); 
       const description = data.weather[0].description;
 
       weatherInfo.innerHTML = `Temperature: ${temperature}°C, Description: ${description}`;
   }
-
-  // Викликати функцію отримання погоди при завантаженні сторінки
   getWeather();
 });
